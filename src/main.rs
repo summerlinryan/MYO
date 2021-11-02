88755001176e8b8cf1cdee63f81bb9bd35171874
Ryan Summerlin <summerlinryan@gmail.com>
2021-11-01 22:12:05 -0400

    Start implementing VGA module (basic printing)

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 #![no_std]
 #![no_main]
 
+mod vga;
+
+use crate::vga::test_printing;
 use core::panic::PanicInfo;
 
 static HELLO: &[u8] = b"Kernel running!";
@@ -12,14 +15,6 @@ fn panic(_info: &PanicInfo) -> ! {
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
-
+    test_printing();
     loop {}
 }
