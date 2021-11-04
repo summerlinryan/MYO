d4571dc39885a0372eaf77b6e1a1ab70304d257f
Ryan Summerlin <summerlinryan@gmail.com>
2021-11-04 18:53:11 -0400

    Add printing to VGA buffer in panic handler

--- src/main.rs
+++ src/main.rs
@@ -8,17 +8,13 @@ use core::panic::PanicInfo;
 static HELLO: &[u8] = b"Kernel running!";
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     use core::fmt::Write;
-
-    for i in 0..50 {
-        println!("Row: {}", i);
-    }
-
-    loop {}
+    panic!("Panic message");
 }
