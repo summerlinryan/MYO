f09cb2a8e82f5d230727fe8b6c406569e24cb6d6
Ryan Summerlin <summerlinryan@gmail.com>
2021-11-08 23:01:27 -0500

    Remove unnecessary `extern crate` statements from core lib

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -3,9 +3,6 @@
 #![test_runner(crate::testing::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
-extern crate lazy_static;
-extern crate spin;
-
 pub mod qemu;
 pub mod serial;
 pub mod test;
